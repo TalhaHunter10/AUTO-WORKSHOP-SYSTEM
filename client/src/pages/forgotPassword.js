a25afc2e846b69bb880d4d87aebe0aebef9985be
Talha Ayub <talhaayub789@gmail.com>
2024-08-19 21:46:01 +0500

    forgotPassword page complete

--- client/src/pages/forgotPassword.js
+++ client/src/pages/forgotPassword.js
@@ -3,9 +3,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../components/button";
 
-const Login = () => {
+const ForgotPassword = () => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
@@ -18,16 +17,16 @@ const Login = () => {
   };
 
   return (
-    <div className="md:mx-20">
+    <div className="mx-8 md:mx-20">
       <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
         <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12 ">
           <div className="mt-10 md:mt-0 pl-0 pr-0 md:pl-10 lg:pl-20 xl:pl-48 md:pr-20 md:mb-20">
-            <h1 className="htext text-4xl md:text-6xl">Login Now</h1>
+            <h1 className="htext text-3xl md:text-5xl">Forgot Password</h1>
             <p className="btext text-xl md:text-2xl mt-6">
-              Hi, Welcome Back &#x1F44B;
+              Enter you registered email address to recieve recovery code.
             </p>
             <form className="mt-10">
-              <p className="btext font-semibold text-xl">Email</p>
+              <p className="btext font-semibold text-xl">Email Address</p>
               <Input
                 placeholder="Enter your email address"
                 type="email"
@@ -35,37 +34,17 @@ const Login = () => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-
-              <p className="btext font-semibold mt-5 text-xl">Password</p>
-              <Input.Password
-                placeholder="Enter your password"
-                className="mt-2 h-12"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
             </form>
-            <p
-              to=""
-              className="btext text-blue-600 font-semibold text-right mt-3 "
-            >
-              <Link
-                to="/forgotpassword"
-                className=" cursor-pointer hover:text-red-600 duration-300"
-              >
-                {" "}
-                Forgot Password ?
-              </Link>
-            </p>
+
             <Button
-              text="Login"
+              text="Send Recovery Code"
               onClick={handleSubmit}
               style="mt-8 mb-5 w-full h-12 text-xl rounded-md"
             />
-            <p className="btext text-center flex justify-center">
-              <span className="font-semibold mr-1">Not registered yet? </span>{" "}
-              Create an Account -{" "}
+            <p className="btext text-center flex justify-left">
+              <span className="font-semibold mr-1">Got your Password? </span>
               <p className="font-semibold text-blue-600 hover:scale-110 duration-300 ml-2">
-                <Link to="/signup"> Sign Up</Link>
+                <Link to="/login"> Login</Link>
               </p>
             </p>
           </div>
@@ -73,7 +52,7 @@ const Login = () => {
 
         <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12 p-10 md:p-20 ">
           <img
-            src="/illustrations/login.png"
+            src="/illustrations/forgotPassword.png"
             className="w-full"
             alt="User Login"
           />
@@ -83,4 +62,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default ForgotPassword;
