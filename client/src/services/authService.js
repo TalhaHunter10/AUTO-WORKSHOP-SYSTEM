64bb5f2edd97b088de809575391fee664d3e2acc
Talha Ayub <talhaayub789@gmail.com>
2024-09-14 23:50:45 +0500

    updated auth service

---
+++ client/src/services/authService.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+export const checkLoginStatus = async () => {
+    try {
+        const response = await axios.get(`${backendUrl}/api/auth/loggedin`)
+        return response;
+    } catch (error) {
+        return error.response.data
+    }
+}
+
+export const registerUser = async (user) => {
+    try {
+        const response = await axios.post(`${backendUrl}/api/auth/register_user`, user)
+        return response;
+    } catch (error) {
+        return error.response.data
+    }
+}
