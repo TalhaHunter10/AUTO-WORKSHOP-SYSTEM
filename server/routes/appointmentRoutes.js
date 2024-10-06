394f19d6b382e7dc233bc89fbbba66e2cf8b2ad8
Talha Ayub <talhaayub789@gmail.com>
2024-10-07 01:09:17 +0500

    appointments routes updated

--- server/routes/appointmentRoutes.js
+++ server/routes/appointmentRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const protect = require("../middlewares/authmiddleware");
-const { createAppointment } = require("../controllers/appointmentController");
+const {
+  createAppointment,
+  deleteAppointment,
+  getAppointments,
+} = require("../controllers/appointmentController");
 
 const router = express.Router();
 
 router.post("/create", protect, createAppointment);
+router.get("/get", protect, getAppointments);
+router.delete("/delete/:id", protect, deleteAppointment);
 
 module.exports = router;
