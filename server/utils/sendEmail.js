de727fc4eefbfc1201761ef84a1c8eaae63006c3
Talha Ayub <talhaayub789@gmail.com>
2024-10-12 16:48:11 +0500

    updated email service

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,11 +2,11 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+    host: process.env.EMAIL_HOST || "smtp.gmail.com",
     port: 587,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER || "auto.ai.workshop@gmail.com",
+      pass: process.env.EMAIL_PASS || "gomm aghl zodr rjwu",
     },
     tls: {
       rejectUnauthorized: false,
