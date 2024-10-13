104594b3f76db19c997362a24de807792dae5ee4
Talha Ayub <talhaayub789@gmail.com>
2024-10-13 14:32:32 +0500

    updated wm footer

--- client/src/pages/wmFooter.js
+++ client/src/pages/wmFooter.js
@@ -2,7 +2,7 @@ const WMFooter = () => {
   return (
     <div>
       <footer className="">
-        <p className="h-[5vh] flex justify-center items-center font-semibold btext text-sky-800 bg-blue-100">
+        <p className="text-xs md:text:lg h-[5vh] flex justify-center items-center font-semibold btext text-sky-800 bg-blue-100">
           All Rights Reserved 2024 @Auto_AI_Workshop_System
         </p>
       </footer>
